5cee1b5314b76392c7ad077a01816c547b5f72ae
Lucas Santos <santosmalachias@gmail.com>
2023-04-15 12:18:18 -0300

    Componentizando card de produtos

--- index.php
+++ index.php
@@ -40,34 +40,13 @@
                     <span class="text-xs fw-light">0 Itens</span>
                 </div>
                 <div>
-                <?php include 'src/components/icons/cart.php'; ?>
+                    <?php include 'src/components/icons/cart.php'; ?>
                 </div>
             </div>
         </nav>
 
         <section class="mt-5">
-        
-            <div class="card-product card" style="width: 18rem;">
-                <div class="card-body">
-                    <img src="./src/images/SANTOS2022.1.jpg" class="rounded shadow card-img-top" alt="Imagem da camisa do Santos 2022">
-                    <div class="d-flex flex-column mt-3">    
-                        <h5 class="card-title fs-6 mb-0">Camisa do Santos 2022</h5>
-                        <span class="fs-4 fw-semibold">R$ 160,00</span>
-
-                        <button type="button" class="btn btn-primary mt-3 d-flex align-items-center p-0 overflow-hidden text-sm">
-                            <div 
-                                class="d-flex align-items-center" 
-                                style="gap: .3rem; background: var(--bs-btn-active-bg); padding: .375rem .5rem;"
-                            >
-                                <?php include 'src/components/icons/add-cart.php'; ?>
-                                0
-                            </div>
-                            <span class="text-uppercase text-center text-xs fw-semibold px-2" style="flex:1">Adicionar ao Carrinho</span>
-                        </button>
-                    </div>  
-                </div>
-            </div>
-
+            <?php include 'src/components/product-item.php'; ?>
         </section>
     </main>
 
