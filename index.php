88fe8c32a03c7a5ff655c6a567eafc7da851d001
Lucas Santos <santosmalachias@gmail.com>
2023-04-15 12:13:01 -0300

    Componentizando icones

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                     <span class="text-xs fw-light">0 Itens</span>
                 </div>
                 <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: currentColor;transform: ;msFilter:;"><path d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z"></path><circle cx="10.5" cy="19.5" r="1.5"></circle><circle cx="17.5" cy="19.5" r="1.5"></circle></svg>
+                <?php include 'src/components/icons/cart.php'; ?>
                 </div>
             </div>
         </nav>
@@ -59,7 +59,7 @@
                                 class="d-flex align-items-center" 
                                 style="gap: .3rem; background: var(--bs-btn-active-bg); padding: .375rem .5rem;"
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="fill: white;transform: ;msFilter:;"><circle cx="10.5" cy="19.5" r="1.5"></circle><circle cx="17.5" cy="19.5" r="1.5"></circle><path d="M13 13h2v-2.99h2.99v-2H15V5.03h-2v2.98h-2.99v2H13V13z"></path><path d="M10 17h8a1 1 0 0 0 .93-.64L21.76 9h-2.14l-2.31 6h-6.64L6.18 4.23A2 2 0 0 0 4.33 3H2v2h2.33l4.75 11.38A1 1 0 0 0 10 17z"></path></svg> 
+                                <?php include 'src/components/icons/add-cart.php'; ?>
                                 0
                             </div>
                             <span class="text-uppercase text-center text-xs fw-semibold px-2" style="flex:1">Adicionar ao Carrinho</span>
