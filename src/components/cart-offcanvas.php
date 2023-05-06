3766c219f6d6b651aa9f0a2cb0d7b9264e3d2e67
Lucas Santos <santosmalachias@gmail.com>
2023-05-06 13:56:52 -0300

    Finalização da função inicial do carrinho

--- src/components/cart-offcanvas.php
+++ src/components/cart-offcanvas.php
@@ -4,36 +4,10 @@
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item py-3 px-1">
-        <div class="d-flex align-items-center column-gap-3">
-          <img 
-            src="https://cdn.shopify.com/s/files/1/1505/8214/products/4_1_1500x.jpg?v=1594931500" 
-            class="rounded shadow card-img-top" 
-            alt="Imagem da camisa do Santos 2022"
-            style="width: 5rem; height: 5rem; object-fit: cover;"
-          >
-          <div class="d-flex flex-column">
-            <h6 class="mb-1">Camisa Malha Dupla</h6>
-            <span class="text-muted">R$ 89,90</span>
-          </div>
-          <div class="ms-auto border roubded d-flex">
-            <button
-              type="button"
-              class="btn m-0 fw-bold text-muted"
-            >+</button>
-            <span class="align-self-center px-1 text-sm text-muted">1</span>
-            <button
-              type="button"
-              class="btn m-0 fw-bold text-muted"
-            >-</button>
-          </div>
-        </div>
-      </li>
-      <li class="list-group-item">A second item</li>
-      <li class="list-group-item">A third item</li>
-      <li class="list-group-item">A fourth item</li>
-      <li class="list-group-item">And a fifth one</li>
+    <ul class="list-group list-group-flush" id="container-cart-products">
+      <li class="list-group-item py-5 px-1 bg-light text-center">
+        <em class="text-sm text-muted">Carrinho vazio</em>
+      </li> 
     </ul>
   </div>
 </div>
---
+++ src/js/cart.js
@@ -0,0 +1,88 @@
+if(!products) console.error('Constante de produtos não encontrado');
+
+var productsOnCart = [];
+function addProductToCart(id){
+    // PROCURAR UM PRODUTO NA LISTA DE PRODUTOS PELO ID
+    const product = products.find((prod) => prod.id === id);
+    if(!product){
+        alert('Produto não encontrado');
+        // PARAR EXECUÇÃO DA FUNÇÃO
+        return;
+    }
+
+    let findedProductOnCart = productsOnCart.find((prod) => prod.id === product.id);
+    if(findedProductOnCart){
+        if (hasQtdInStock(product, findedProductOnCart.qtd +1)){
+           findedProductOnCart.qtd++;
+        }  
+        else{
+            alert('Não há mais este produto em estoque');
+            return;
+        }       
+    }
+    else productsOnCart.push({
+        ...product,
+        qtd: 1
+    });
+    
+    renderProductsOnCart();
+
+    if (!document.querySelector('#cartOffcanvas.show')){
+        document.querySelector('[data-bs-target="#cartOffcanvas"]').click()
+    }
+}
+function removeProductToCart(id){
+    let findedProductOnCart = productsOnCart.find((prod) => prod.id === id);
+    if(!findedProductOnCart){
+      alert('Produto não está mais no carrinho');
+      return;
+    }
+    
+    if(findedProductOnCart.qtd <= 1){
+      productsOnCart = productsOnCart.filter((prod) => prod.id !== id);
+    }
+    else findedProductOnCart.qtd--;
+  
+    renderProductsOnCart();
+}
+function hasQtdInStock(product, qtd){
+    return product.stock_qtd >= qtd;
+}
+function renderProductsOnCart(){
+    let container = document.getElementById('container-cart-products');
+
+    if(productsOnCart.length === 0) container.innerHTML = `
+        <li class="list-group-item py-5 px-1 bg-light text-center">
+            <em class="text-sm text-muted">Carrinho vazio</em>
+        </li>    
+    `;
+    else container.innerHTML = productsOnCart.map(product => `
+        <li class="list-group-item py-3 px-1">
+            <div class="d-flex align-items-center column-gap-3">
+                <img 
+                    src="${product.image}" 
+                    class="rounded shadow card-img-top" 
+                    alt="Imagem da camisa do Santos 2022"
+                    style="width: 5rem; height: 5rem; object-fit: cover;"
+                >
+                <div class="d-flex flex-column">
+                    <h6 class="mb-1">${product.name}</h6>
+                    <span class="text-muted">${product.price_formatted}</span>
+                </div>
+                <div class="ms-auto border roubded d-flex">
+                    <button
+                        type="button"
+                        class="btn m-0 fw-bold text-muted"
+                        onclick="removeProductToCart(${product.id})"    
+                    >-</button>
+                    <span class="align-self-center px-1 text-sm text-muted">${product.qtd}</span>
+                    <button
+                        type="button"
+                        class="btn m-0 fw-bold text-muted"
+                        onclick="addProductToCart(${product.id})"
+                    >+</button>
+                </div>
+            </div>
+        </li>
+    `).join(' ');
+}
